5e7d5dca7851c71a326663254e0d0d083e84cc33
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2026-04-07 18:52:41 +0300

    chore: update theme folder

--- ckanext/midnight_blue_theme/plugin.py
+++ ckanext/midnight_blue_theme/plugin.py
@@ -1,16 +1,20 @@
-import ckan.plugins as plugins
-import ckan.plugins.toolkit as toolkit
+from __future__ import annotations
+
+from typing_extensions import override
+
+import ckan.plugins as p
+import ckan.plugins.toolkit as tk
+
+from ckanext.theming.interfaces import ITheme
+from ckanext.theming.lib import Theme
+
+from ckanext.midnight_blue_theme.themes.midnight_blue_portable.theme import make_theme
+
+from . import helpers
 
 
-class MidnightBlueThemePlugin(plugins.SingletonPlugin):
-    plugins.implements(plugins.IConfigurer)
-    
-
-    # IConfigurer
-
-    def update_config(self, config_):
-        toolkit.add_template_directory(config_, "templates")
-        toolkit.add_public_directory(config_, "public")
-        toolkit.add_resource("assets", "midnight_blue_theme")
-
-    
+@tk.blanket.helpers(helpers.get_helpers)
+class MidnightBlueThemePlugin(ITheme, p.SingletonPlugin):
+    @override
+    def register_themes(self) -> list[Theme]:
+        return [make_theme()]
