57b25538c10308874312f5defe5adc064dd76640
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2026-06-09 21:16:27 +0300

    chore: use vite iife

--- ckanext/midnight_blue_theme/themes/mbp/vite.config.ts
+++ ckanext/midnight_blue_theme/themes/mbp/vite.config.ts
@@ -1,19 +1,14 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
-import checker from "vite-plugin-checker";
 
 export default defineConfig({
-    // plugins: [
-    //     checker({
-    //         typescript: true,
-    //     }),
-    // ],
     publicDir: false,
     build: {
         lib: {
             entry: resolve(__dirname, "assets/src/main.ts"),
-            formats: ["es"],
+            formats: ["iife"],
             fileName: (format) => `ui.js`,
+            name: "ThemingUI",
         },
         outDir: "assets/javascript",
         emptyOutDir: false,
